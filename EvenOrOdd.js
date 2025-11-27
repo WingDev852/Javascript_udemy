cb76b0193dd15cc268b4ab3f5b3f390a5038a6a1
Wing_Dev <wing_lai_dev@hotmail.com>
2025-11-27 10:35:40 +0100

    check if number even or odd

---
+++ EvenOrOdd.js
@@ -0,0 +1,10 @@
+function evenOrOdd(number) {
+    if (number % 2 === 0) {
+        return "Your number is even"
+    } else {
+        return "Your number is odd"
+    }
+}
+
+
+console.log(evenOrOdd(12))
